87dfd1d749731d23e7d1d84f8cc0f37eb6bab10b
magrega <proshanov@yandex.ru>
2023-06-26 18:58:15 +0300

    cardPage work done

--- src/components/CardPage/CardPage.tsx
+++ src/components/CardPage/CardPage.tsx
@@ -18,24 +18,24 @@ const CardPage: FC = () => {
                 <span className='cardpage__closebtn-wrapper'>
                     <img className='cardpage__closebtn-wrapper-img' src={require(`../../assets/icons/backArrow.svg`).default} alt="back arrow" />Назад</span>
             </button>
+            <h2 className='cardpage__h2'>{`${cards[index].totem}`}</h2>
             <div className="cardpage__body">
-                <div className="cardpage__video-container">
-                    <video className="cardpage__card-vid"
-                        poster={require(`../../assets/img/${id}.webp`)}
-                        autoPlay
-                        loop
-                        playsInline
-                        muted
-                        src={require(`../../assets/video/${id}.mp4`)} />
-                </div>
-                <div className='cardpage__card-text'>
-                    <p className='cardpage__p' >{`Тотем силы: ${cards[index].totem}`}</p>
-                    <p className='cardpage__p' >{`Буква Иврита: ${cards[index].letter}`}</p>
-                    <p className='cardpage__p' >{`Значение: ${cards[index].meaning}`}</p>
-                    <p className='cardpage__p' >{`Элемент: ${cards[index].element}`}</p>
-                    <p className='cardpage__p' >{`Символы: ${cards[index].symbols}`}</p>
-                    <p className='cardpage__p description' >{`Описание: ${cards[index].description}`}</p>
-                </div>
+                <video className="cardpage__card-vid"
+                    poster={require(`../../assets/img/${id}.webp`)}
+                    autoPlay
+                    loop
+                    playsInline
+                    muted
+                    src={require(`../../assets/video/${id}.mp4`)} />
+                <p className="text-align-left"><span className='cardpage__text-category'>Буква Иврита: </span>{`${cards[index].letter}`}</p>
+                <br />
+                <p className="text-align-left"><span className='cardpage__text-category'>Значение: </span>{`${cards[index].meaning}`}</p>
+                <br />
+                <p className="text-align-left"><span className='cardpage__text-category'>Элемент: </span>{`${cards[index].element}`}</p>
+                <br />
+                <p className="text-align-left"><span className='cardpage__text-category'>Символы: </span>{`${cards[index].symbols}`}</p>
+                <br />
+                <span className="cardpage__text-description"><span className='cardpage__text-category'>Описание: </span>{`${cards[index].description}`}</span>
             </div>
         </Box>
     );
--- src/components/router.js
+++ src/components/router.js
@@ -1,24 +1,23 @@
-import { createBrowserRouter, createRoutesFromElements, Route, Navigate } from 'react-router-dom';
-import Layout from './Layout/Layout';
-import Gallery, { cardLoader } from './Gallery/Gallery';
-import WorkGallery from './WorkGallery/WorkGallery';
+import { Navigate, Route, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
 import CardPage from './CardPage/CardPage';
+import Gallery, { cardLoader } from './Gallery/Gallery';
+import Layout from './Layout/Layout';
+import WorkGallery from './WorkGallery/WorkGallery';
 
 export const router = createBrowserRouter(createRoutesFromElements(
-  <Route path='/' element={<Layout />}>
-    <Route path='/intro' element={<Gallery />} loader={cardLoader} />
-    <Route path='/interact' element={<WorkGallery />} />
-    <Route
-      path="/"
-      element={<Navigate to="/intro" replace />}
-    />
-    <Route
-      path="/shulamit"
-      element={<Navigate to="/intro" replace />}
-    />
-    <Route 
-    path="/intro/card/:id" 
-    element={<CardPage/>} />
-
-  </Route>
+    <Route path='/' element={<Layout />}>
+      <Route path='/intro' element={<Gallery />} loader={cardLoader} />
+      <Route path='/interact' element={<WorkGallery />} />
+      <Route
+        path="/"
+        element={<Navigate to="/intro" replace />}
+      />
+      <Route
+        path="/shulamit"
+        element={<Navigate to="/intro" replace />}
+      />
+      <Route
+        path="/intro/card/:id"
+        element={<CardPage />} />
+    </Route>
 ))
