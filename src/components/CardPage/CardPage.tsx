ff4a5c9aa768115349e83e557f7c77706c045754
magrega <proshanov@yandex.ru>
2023-06-26 22:30:51 +0300

    cardPage work done

--- src/components/CardPage/CardPage.tsx
+++ src/components/CardPage/CardPage.tsx
@@ -26,10 +26,16 @@ const CardPage: FC = () => {
 
     return (
         <Box className='cardpage'>
-            <button type="button" className="cardpage__btn" onClick={goBack}>
+            <button className="cardpage__btn" onClick={goBack}>
                 <span className='cardpage__closebtn-wrapper'>
-                    <img className='cardpage__closebtn-wrapper-img' src={require(`../../assets/icons/backArrow.svg`).default} alt="back arrow" />Назад</span>
+                    <img className='cardpage__closebtn-wrapper-img' src={require(`../../assets/icons/backArrow.svg`).default} alt="back arrow" />
+                    Назад
+                </span>
             </button>
+            <div className="cardpage-buttons">
+                <button className={id && +id === 1 ? 'cardpage__btn inactive' : 'cardpage__btn'} onClick={goPrev}><img className='cardpage__prevbtn' src={require(`../../assets/icons/cardNextPrev.svg`).default} alt="Prev card" /></button>
+                <button className={id && +id === cards.length ? 'cardpage__btn inactive' : 'cardpage__btn'} onClick={goNext}><img className='cardpage__nextbtn' src={require(`../../assets/icons/cardNextPrev.svg`).default} alt="Next card" /></button>
+            </div>
             <h2 className='cardpage__h2'>{`${cards[index].totem}`}</h2>
             <div className="cardpage__body">
                 <video className="cardpage__card-vid"
@@ -44,10 +50,6 @@ const CardPage: FC = () => {
                 <p className="cardpage__text"><span className='cardpage__text-category'>Элемент: </span>{`${cards[index].element}`}</p>
                 <p className="cardpage__text"><span className='cardpage__text-category'>Символы: </span>{`${cards[index].symbols}`}</p>
                 <span className="cardpage__text-description"><span className='cardpage__text-category'>Описание: </span>{`${cards[index].description}`}</span>
-            </div>
-            <div className="cardpage-buttons">
-                <button className={id && +id === 1 ? 'cardpage__btn inactive' : 'cardpage__btn'} onClick={goPrev}><img className='cardpage__prevbtn' src={require(`../../assets/icons/cardNextPrev.svg`).default} alt="Prev card" /></button>
-                <button className={id && +id === cards.length ? 'cardpage__btn inactive' : 'cardpage__btn'} onClick={goNext}><img className='cardpage__nextbtn' src={require(`../../assets/icons/cardNextPrev.svg`).default} alt="Next card" /></button>
             </div>
         </Box>
     );
