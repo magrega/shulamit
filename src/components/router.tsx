76af203dc9b05547cb9f335b5a791f7917ed1b8b
magrega <proshanov@yandex.ru>
2023-07-09 17:22:43 +0300

    added pulse to cardPage video

--- src/components/router.tsx
+++ src/components/router.tsx
@@ -14,6 +14,10 @@ export const router = createBrowserRouter(createRoutesFromElements(
     <Route
       path="/card/:id"
       element={<CardPage />} />
+      <Route
+      path="/shulamit"
+      element={<Navigate to="/intro" replace />}
+    />
     <Route path="*" element={<Navigate to="/intro" replace />} />
   </Route>
 ))
