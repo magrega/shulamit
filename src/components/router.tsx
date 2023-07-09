54e4b8c121da012ca998d0ee4ea8d127be4d9a94
magrega <proshanov@yandex.ru>
2023-07-09 16:11:13 +0300

    css minor fixes

--- src/components/router.tsx
+++ src/components/router.tsx
@@ -1,18 +1,14 @@
-import { Navigate, Route, createHashRouter, createRoutesFromElements } from 'react-router-dom';
+import { Navigate, Route, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
 import CardPage from './CardPage/CardPage';
 import Gallery from './Gallery/Gallery';
 import Layout from './Layout/Layout';
 
-export const router = createHashRouter(createRoutesFromElements(
+export const router = createBrowserRouter(createRoutesFromElements(
   <Route path='/' element={<Layout />}>
-    <Route path='/intro' element={<Gallery isCardBack={false} />} />
+    <Route index path='/intro' element={<Gallery isCardBack={false} />} />
     <Route path='/interact' element={<Gallery isCardBack={true} />} />
     <Route
       path="/"
-      element={<Navigate to="/intro" replace />}
-    />
-    <Route
-      path="/shulamit"
       element={<Navigate to="/intro" replace />}
     />
     <Route
