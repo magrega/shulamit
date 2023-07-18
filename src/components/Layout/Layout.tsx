380b4fe4770ec7c72c01692a61f2bc66f27d55bd
magrega <proshanov@yandex.ru>
2023-07-18 10:43:57 +0300

    added link to metacast

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -19,6 +19,7 @@ const Layout = () => {
                     <a href="https://www.instagram.com/shulanika/"><img className='socials-container__item' src={require("../../assets/icons/instagram.png")} alt="insta" /></a>
                     <a href="https://t.me/nikashulanika"><img className='socials-container__item' src={require("../../assets/icons/telegram.png")} alt="telegram" /></a>
                     <a href="https://www.youtube.com/@shulanika"><img className='socials-container__item' src={require("../../assets/icons/youtube.png")} alt="youtube" /></a>
+                    <a href="https://metabroadcast.ru"><img className='socials-container__item' src={require("../../assets/icons/code.png")} alt="made by" /></a>
                 </div>
             </footer>
         </>
