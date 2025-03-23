6fc55b0ceeccf77cf25367aa0d0f033cc52252b2
Alexander Proshanov <proshanov@yandex.ru>
2025-03-23 18:11:01 +0300

    refactor: Clean up ftpDeploy.js after testing

--- ftpDeploy.js
+++ ftpDeploy.js
@@ -3,26 +3,20 @@ const ftpDeploy = new FtpDeploy();
 
 const config = {
   user: process.env.REACT_APP_FTP_USERNAME,
-  // Password optional, prompted if none given
   password: process.env.REACT_APP_FTP_PASSWORD,
   host: process.env.REACT_APP_FTP_HOST,
   port: 22,
   localRoot: __dirname + "\\dist",
   remoteRoot: "/root/websites/shulamit",
-  // include: ["*", "**/*"],      // this would upload everything except dot files
   include: ["*"],
-  // e.g. exclude sourcemaps, and ALL files in node_modules (including dot files)
   exclude: [
     "dist/**/*.map",
     "node_modules/**",
     "node_modules/**/.*",
     ".git/**",
   ],
-  // delete ALL existing files at destination before uploading, if true
   deleteRemote: true,
-  // Passive mode is forced (EPSV command is not sent)
   forcePasv: true,
-  // use sftp or ftp
   sftp: true,
 };
 
