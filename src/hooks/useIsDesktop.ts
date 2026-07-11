1d3b8458d3a9316f7cc6185fca28d8fe85a3521d
magrega <proshanov@yandex.ru>
2026-07-11 19:01:23 +0300

    refactor: refactor useIsDesktop to use matchMedia

--- src/hooks/useIsDesktop.ts
+++ src/hooks/useIsDesktop.ts
@@ -1,15 +1,19 @@
 import { useEffect, useState } from "react";
 
+const query = `(min-width: 870px)`;
+
 export const useIsDesktop = () => {
-  const breakpoint = 870;
-  const [isDesktop, setIsDesktop] = useState(window.innerWidth > breakpoint);
+  const [isDesktop, setIsDesktop] = useState(
+    () => window.matchMedia(query).matches,
+  );
 
   useEffect(() => {
-    const handleResize = () =>
-      window.innerWidth > breakpoint ? setIsDesktop(true) : setIsDesktop(false);
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
-  }, [setIsDesktop]);
+    const mql = window.matchMedia(query);
+    const onChange = (e: MediaQueryListEvent) => setIsDesktop(e.matches);
+    mql.addEventListener("change", onChange);
+    setIsDesktop(mql.matches);
+    return () => mql.removeEventListener("change", onChange);
+  }, []);
 
   return { isDesktop };
 };
